5f6fd598fd12cd14567f5f456d2165efb2d17071
afoyer <lemimic213@gmail.com>
2021-02-07 22:25:49 -0600

    updated style (more in progress)

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,16 @@ export default function Home() {
       {session && (
         <>
           <nav className="navigation">
-            <div>Hello {session.user.name}</div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, delay: 0.2 },
+              }}
+            >
+              Hello {session.user.name}
+            </motion.div>
             <button
               className="btn btn-primary"
               onClick={() => {
